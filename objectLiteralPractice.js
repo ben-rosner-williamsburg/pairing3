4b9e49f2568d479658cf89f4de020d3a934faffc
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2023-05-21 14:02:10 -0400

    Create person object

---
+++ objectLiteralPractice.js
@@ -0,0 +1,12 @@
+var person = {
+    name: "Ben Rosner",
+    isMarried: false,
+    favColor: "red",
+    favSport: "soccer"
+}
+
+console.log(person);
+console.log(person.name);
+person.eyeColor = "green";
+person.hairColor = "brown";
+console.log(Object.keys(person));
